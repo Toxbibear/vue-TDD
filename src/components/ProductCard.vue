fc4b8bf1785f7d0467aad71a64e6311ee0e93268
Toxbibear <iliasse.ilia@gmail.com>
2025-03-27 15:29:11 +0100

    passed Product Card/ProductStore tests

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,24 +1,30 @@
 <script setup>
-const props = defineProps(
-    name: String,
-    price: Number,
-    rating: Number,
-    img: String,
-);
+const props = defineProps({
+  product: {
+    type: Object,
+    required: true
+  }
+});
+
+const emit = defineEmits(['add-to-cart']);
+
+const addToCart = () => {
+  emit('add-to-cart', props.product);
+}
 </script>
 
 <template>
-    <div class="productCard">
-        <div class="productCardContainer">
-            <img src={{img}} alt="">
-            <div class="productCardTextContainer">
-                <h3>{{name}}</h3>
-                <div name="price" class="price">{{price}}</div>
-                <div class="rating">{{rating}}</div>
-            </div>
-        </div>
+  <div class="productCard">
+    <div class="productCardContainer">
+      <img :src="product.images[0]" :alt="product.name">
+      <div class="productCardTextContainer">
+        <h3>{{ product.name }}</h3>
+        <div class="price">${{ product.price.toFixed(2) }}</div>
+        <div class="rating">{{ product.rating }}</div>
+      </div>
     </div>
-
+    <button @click="addToCart">Add to Cart</button>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/ProductFilter.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({
+  products: {
+    type: Array,
+    required: true
+  }
+});
+</script>
+
+    
+<template>
+
+</template>
+<style scoped></style>
